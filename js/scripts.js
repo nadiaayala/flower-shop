e41f996f7ded429ba8838ac1618fefed02b4d12d
Nadia Ayala <nadiaayalanogueira@outlook.com>
2018-12-08 13:50:57 +0400

    Site styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
-var btnRose = document.getElementById("flowers");
+var tulips = document.getElementById('tulips');
 var sunflowers = document.getElementById('sunflowers');
+var roses = document.getElementById('roses');
 
-btnRose.addEventListener('click', function(){
+
+sunflowers.addEventListener('click', function(){   
+    var actives = document.getElementsByClassName('active');
+    console.log(actives);
+    while(actives.length>0){
+        actives[0].classList.remove('active');
+    }
+    this.classList.add('active');
     var request = new XMLHttpRequest;
     request.onload = function () {
         if (request.status === 200) {
@@ -19,3 +27,52 @@ btnRose.addEventListener('click', function(){
     request.open('GET', 'data/data.json', true);
     request.send(null);
 })
+
+tulips.addEventListener('click', function(){
+    var actives = document.getElementsByClassName('active');
+    while(actives.length>0){
+        actives[0].classList.remove('active');
+    }
+    this.classList.add('active');
+    var request = new XMLHttpRequest;
+    request.onload = function () {
+        if (request.status === 200) {
+            responseObject = JSON.parse(request.responseText);
+            var content = '';
+            for (var i = 0; i < responseObject.events.length; i++) {
+                content += '<div>';
+                content += '<img src="' + responseObject.events[i].image + '">';
+                content += '<p>' + responseObject.events[i].name + '</p>';
+                content += '</div>';
+            }
+            document.getElementById('div-content').innerHTML = content +"<br>";
+        }
+    };
+    request.open('GET', 'data/data2.json', true);
+    request.send(null);
+})
+
+roses.addEventListener('click', function(){
+    var actives = document.getElementsByClassName('active');
+    while(actives.length>0){
+        actives[0].classList.remove('active');
+    }
+    this.classList.add('active');   
+    var request = new XMLHttpRequest;
+    request.onload = function () {
+        if (request.status === 200) {
+            responseObject = JSON.parse(request.responseText);
+            var content = '';
+            for (var i = 0; i < responseObject.events.length; i++) {
+                content += '<div>';
+                content += '<img src="' + responseObject.events[i].image + '">';
+                content += '<p>' + responseObject.events[i].name + '</p>';
+                content += '</div>';
+            }
+            document.getElementById('div-content').innerHTML = content +"<br>";
+        }
+    };
+    request.open('GET', 'data/data3.json', true);
+    request.send(null);
+})
+
